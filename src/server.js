e04db2c3d54c056bc760d89e1870406e5313bd28
imooje <savvyplus020@gmail.com>
2025-04-24 21:11:03 +0100

    fix upload error

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const multer = require("multer");
 
 const app = express();
 const port = 8000;
 const baseDir = require("os").homedir(); // Operating system's home directory
+const uploadDir = path.join(baseDir, "uploads");
+
+if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+
+const storage = multer.diskStorage({
+  destination: (_, __, callback) => callback(null, uploadDir),
+  filename: (_, file, callback) =>
+    callback(null, Date.now() + "-" + file.originalname),
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.static(uploadDir));
 
 // Index page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/upload", upload.array("files"), (req, res) => {
+  res.send("File uploaded successfully!");
 });
 
 // API: List PC files and folders
